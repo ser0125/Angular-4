dce09253bd48c3d184a808fd97575e87a7f55e70
Sergio Andres Llanos Garcia <sergio.llanos@udea.edu.co>
2017-09-18 16:23:09 -0500

    Finish the first part of the proyect

--- prj-start/prj-start/src/app/shooping-list/shooping-list.component.ts
+++ prj-start/prj-start/src/app/shooping-list/shooping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Ingredient} from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shooping-list',
   templateUrl: './shooping-list.component.html',
   styleUrls: ['./shooping-list.component.css']
 })
 export class ShoopingListComponent implements OnInit {
+		ingredients: Ingredient[] = [
+			new Ingredient('Apples', 5),
+			new Ingredient('tomatoes', 15),
+		];
 
   constructor() { }
 
